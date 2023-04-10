7b069a971390c32e3bc6b956ccc0a06005341cc3
ShwetaChavan1809 <shweta.chavan95@gmail.com>
2023-04-10 14:11:55 -0700

    jar file execution added to stert-up script

--- start-up-script/start_elk.sh
+++ start-up-script/start_elk.sh
@@ -8,3 +8,15 @@ sudo systemctl start logstash
 
 # Start Kibana
 sudo systemctl start kibana
+
+# Change to the directory where your Spring Boot application is located
+cd Anomaly-Detection/log-collector-application/Bank-Management-Spring-ELK/
+
+# Build the Spring Boot application with Maven
+mvn clean package
+
+# Change to the java jar directory
+cd target
+
+#Run the jar file for the Java application
+java -jar ElkApplication-0.0.1-SNAPSHOT.jar
